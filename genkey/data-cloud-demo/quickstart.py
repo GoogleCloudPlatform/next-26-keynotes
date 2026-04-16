d7602d0fd13d1619072f93c71fd2c2b905564a89
Abirami Sukumaran <abidsukumaran@gmail.com>
2026-04-16 14:10:00 +0530

    Added license on top

--- genkey/data-cloud-demo/quickstart.py
+++ genkey/data-cloud-demo/quickstart.py
@@ -1,3 +1,17 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Register_Existing_Data").getOrCreate()
@@ -37,5 +51,5 @@ for file_name in files:
 print("All tables registered successfully!")
 
 
-# Verify one table
-#spark.sql("SELECT * FROM acaibucket.acai_dataset.order_items LIMIT 5").show()
+# Verify one table in BigQuery
+# SELECT * FROM <<namespace>>.<<dataset>>.order_items LIMIT 5;
