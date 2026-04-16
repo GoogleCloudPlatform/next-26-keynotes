333205893f67592c7f66159bd0fd95544ad27a26
artip90 <artiprasad@google.com>
2026-04-16 10:05:55 +0530

    Fix namespace and dataset names in quickstart.py
    Updated namespace and dataset references in the script.

--- genkey/data-cloud-demo/quickstart.py
+++ genkey/data-cloud-demo/quickstart.py
@@ -14,8 +14,10 @@ files = [
 ]
 
 # 1. CRITICAL: Create the namespace first
-# This maps to the BigQuery dataset.
-spark.sql("CREATE NAMESPACE IF NOT EXISTS <BUCKET_NAME>.acai_dataset")
+# This maps to the BigQuery dataset. You can replace the namespace name (acai_data) and dataset (acai_dataset) to whatever you want.
+# But make sure to reference the same in line 31.
+
+spark.sql("CREATE NAMESPACE IF NOT EXISTS acai_data.acai_dataset")
 
 # Loop through and register each as an Iceberg table in BigLake
 for file_name in files:
@@ -26,7 +28,7 @@ for file_name in files:
 
    # 3. Write to the catalog.
   # Note: Use .tableProperty inside the writeTo chain correctly
-  df.writeTo(f"acai_demo.acai_dataset.{file_name}") \
+  df.writeTo(f"acai_data.acai_dataset.{file_name}") \
       .using("iceberg") \
       .tableProperty("write.format.default", "parquet") \
       .createOrReplace()
